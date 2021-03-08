01b305537ec5289317d37ec56b6ce428d7f5ab13
Cgy-codelab <18883993985@163.com>
2021-03-08 09:38:44 +0800

    first

---
+++ src/views/SelectInput.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="box">
+    
+      <div class="txt">
+        <span v-for="(item,index) in dataList" :key="index" v-if="item.flag">
+          {{item.text}}
+           <a href="#" @click="dele(index)">×</a>
+        </span>
+        
+        <div class="add" @click="addItems">+</div>
+      </div>
+      <div v-if="this.view" class="shadow">
+        <div class="selectitem">
+          <input class="search" type="text" placeholder="搜索">
+        </div>
+        <div class="check">
+          <span v-for="(item,index) in dataList" :key="index">
+            <input type="checkbox" :checked="item.flag"  @click="change(index)">
+            {{item.text}}
+            </span>
+                 
+        </div>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+      return {
+        view:false,
+        dataList:[
+          {text:'变更中',flag:false},
+          {text:'未生效',flag:false},
+          {text:'作废',flag:false},
+          {text:'正常',flag:false},
+          {text:'审核',flag:false},
+        ]
+      }
+    },
+  methods: {
+    change(val){
+       this.dataList[val].flag=!this.dataList[val].flag   
+       },
+    
+    addItems(){
+      this.view=!this.view
+    },
+    dele(val){
+      this.dataList[val].flag=!this.dataList[val].flag  
+    }
+  }
+  
+};
+</script>
+<style scoped>
+.box{
+  height: 26px;
+  width: 300px;
+  box-sizing: border-box;
+}
+.txt{
+  line-height: 30px;
+  width: 300px;
+  height: 30px;
+  border: 1px solid grey;
+  position: relative;
+}
+.txt span{
+  display: inline-block;
+  width: 50px;
+  font-size: 10px;
+  height: 20px;
+  text-align: center;
+  line-height: 20px;
+  background-color:#0066FF ;
+  color: white;
+  border-radius: 8px;
+  margin-left: 3px;
+}
+a{
+  text-decoration: none;
+  color: white;
+}
+.shadow{
+  box-shadow: 0px 7px 15px rgba(1, 1, 1, .3);
+}
+.add{
+  cursor: pointer;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  text-align: center;
+  line-height: 20px;
+  width: 20px;
+  height: 20px;
+  border: 1px solid grey;
+  border-radius: 50%;
+  font-weight: bold;
+  color:grey;
+
+}
+.selectitem{
+  width: 302px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(177, 177, 177);
+  
+}
+.search{
+  outline: none;
+  padding-left: 10px;
+  width: 280px;
+  border-radius: 10px;
+  border: 1px solid rgb(177, 177, 177);
+}
+.check{
+  width: 300px;
+  border: 1px solid rgb(177, 177, 177);
+  display: flex;
+  flex-direction: column;
+}
+.check span{
+  font-size: 10px;
+  padding-bottom: 5px;
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/views/Table.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <!--冻结头 -->
+    <table class="head">
+      <tr>
+        <th>日期</th>
+        <th>名字</th>
+        <th class="address">地址</th>
+      </tr>
+    </table>
+
+    <!-- 滑动部分 -->
+    <div class="body">
+      <table>
+        <tr v-for="(item, index) in itemList" :key="index">
+          <td>{{ item.date }}</td>
+          <td>{{ item.name }}</td>
+          <td class="address">{{ item.address }}</td>
+        </tr>
+      </table>
+    </div>
+  
+  <h1>------------------------------------------------------------------------</h1>
+  <!-- 下拉菜单 -->
+  <select-input></select-input>
+  </div>
+</template>
+
+<script>
+import SelectInput from '../views/SelectInput.vue'
+export default {
+  components:{
+    SelectInput,
+  },
+  data() {
+    return {
+      itemList: [
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+        {name:'王小虎',date:'2021-3-7',address:'重庆市南岸区重庆邮电大学'},
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.head {
+  width: 1000px;
+  border-collapse: collapse;
+}
+.body {
+  height: 300px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  border-bottom: 1px grey solid;
+  width: 1000px;
+}
+.body table {
+  border-collapse: collapse;
+  width: 1000px;
+  height: 300px;
+}
+th {
+  color: grey;
+  height: 50px;
+  width: 100px;
+  border: 1px grey solid;
+}
+td {
+  width: 100px;
+  text-align: center;
+  height: 50px;
+  border: 1px grey solid;
+}
+.address {
+  width: 300px;
+}
+</style>
